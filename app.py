32e7e06c51c9900d76ee08d2b178981270216f19
fish895623 <dan990429@gmail.com>
2022-04-22 14:34:01 +0900

    Add requirements.txt

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, jsonify, render_template, request
+from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
 
 UPLOAD_DIRECTORY = './tmp'
@@ -11,18 +11,11 @@ if not os.path.exists(UPLOAD_DIRECTORY):
 app = Flask(__name__)
 
 
-# @app.route('/receive', methods=['post'])
-# def form():
-#     file = request.files['file']
-#     filename = secure_filename(file.filename)
-#     file.save(os.path.join(UPLOAD_DIRECTORY, filename))
-#     return jsonify({'status': 'success'})
-
-
 @app.route('/receive', methods=['post'])
 def form():
     file = request.files['file']
     filename = secure_filename(file.filename)
+    app.logger(filename)
     file.save(os.path.join(UPLOAD_DIRECTORY, filename))
     return 'hello'
 
