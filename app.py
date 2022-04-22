62a5ac1f9ec044bf66cae311fd521f3bed0a4912
fish895623 <dan990429@gmail.com>
2022-04-22 10:00:35 +0900

    static testing

--- app.py
+++ app.py
@@ -1,23 +1,28 @@
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, render_template, request
 from werkzeug.utils import secure_filename
 
-UPLOAD_DIRECTORY = "./tmp"
+UPLOAD_DIRECTORY = './tmp'
 
 if not os.path.exists(UPLOAD_DIRECTORY):
     os.mkdir(UPLOAD_DIRECTORY)
 
 app = Flask(__name__)
 
-@app.route("/receive", methods=['post'])
+
+@app.route('/receive', methods=['post'])
 def form():
     file = request.files['file']
     filename = secure_filename(file.filename)
     file.save(os.path.join(UPLOAD_DIRECTORY, filename))
-    return jsonify({"status": 'success'})
+    return jsonify({'status': 'success'})
 
+
+@app.route('/', methods=['GET'])
+def hello():
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ static/index.js
@@ -0,0 +1,4 @@
+const t = document.getElementById('start');
+t.addEventListener('click', ()=>{
+    console.log("hello")
+})
